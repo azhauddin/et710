f2684c52e5ea3d6ac224b6ae7105b44110be820f
azhauddin <azhauddinkam@icloud.com>
2016-05-12 15:14:45 -0400

    mid class update

---
+++ site/js/_vti_cnf/foundation.js
@@ -0,0 +1,9 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|12 May 2016 18:26:00 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_author:SR|ET\\Instructor
+vti_modifiedby:SR|ET\\Instructor
+vti_timecreated:TR|16 Feb 2016 15:57:13 -0000
+vti_backlinkinfo:VX|
+vti_cacheddtm:TX|12 May 2016 18:26:00 -0000
+vti_filesize:IR|256105
